6f095ad06932555efb5baeb5b6bd6bd2e0b08a70
talenaa <talenabarbosa7@gmail.com>
2025-09-16 18:43:28 -0300

    feat: assistant file for position

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 import pyautogui
+import time
 
-pyautogui.PAUSE = 2.0
+pyautogui.PAUSE = 2
 
 pyautogui.press("win")
 pyautogui.write("chrome")
@@ -8,3 +9,9 @@ pyautogui.press("enter")
 
 pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press("enter")
+
+time.sleep(3)
+
+pyautogui.click(x=910, y=370)
+
+pyautogui.write("talenabarbosa7@gmail.com")
