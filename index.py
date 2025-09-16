409dc5142f023885693c269e70c6307dfbdfd6b2
talenaa <talenabarbosa7@gmail.com>
2025-09-16 15:55:51 -0300

    feat: first steps of the database automation

---
+++ index.py
@@ -0,0 +1,10 @@
+import pyautogui
+
+pyautogui.PAUSE = 2.0
+
+pyautogui.press("win")
+pyautogui.write("chrome")
+pyautogui.press("enter")
+
+pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
+pyautogui.press("enter")
